1ca58c57d7184f97b6fa8d88498c789b95720aec
LightGuard <lightguard.jp@gmail.com>
2014-05-09 13:27:20 -0600

    Fixes DEVELOPER-464 (new urls)
    This should fix the urls for when we move to <project>.jboss.org, not
    sure if you want to hold off on merging this until that time or not
    though.

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -1,12 +1,22 @@
 ---
 interpolate: true
 ---
-function getCorrectUrl(linkUrl) {
+function getCorrectUrl(linkUrl, path) {
+  console.log("linkUrl:" + linkUrl + " path:" + path);
+  if (path) {
+    if (path.contains(linkUrl)) {
+      if (path.contains('http')) > 0) {
+        return path;
+      } else {
+        return "https://" + linkUrl + ".jboss.org" + path.split(linkUrl)[1];
+      }
+    } else {
+      return "https://" + linkUrl + ".jboss.org" + path;
+    }
+  }
   if (linkUrl.indexOf("/") > 0) {
     return linkUrl;
-  } else {
-    return "https://jboss.org" + linkUrl;
-  }
+  } 
 }
 
 app.project = {
@@ -84,74 +94,74 @@ app.project = {
 
         if (props.downloadsLink) {
           template += "<a class=\"upstream-download\" href=\""
-            + getCorrectUrl(props.downloadsLink) + "\"><i class=\"fa fa-download\"></i> Download</a>";
+            + getCorrectUrl(props.sys_url_view, props.downloadsLink) + "\"><i class=\"fa fa-download\"></i> Download</a>";
         }
         template += "<div class=\"upstream-more-content\">"
           // + "<p class=\"product-links\">Included in Product(s)<br></p>"
           + "<ul class=\"external-links\">";
 
         if (props.docsLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.docsLink) + "\">Documentation</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.docsLink) + "\">Documentation</a></li>";
         }
         if (props.communityLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.communityLink) + "\">Community</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.communityLink) + "\">Community</a></li>";
         }
         if (props.knowledgeBaseLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.knowledgeBaseLink) + "\">KnowledgeBase</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.knowledgeBaseLink) + "\">KnowledgeBase</a></li>";
         }
         if (props.userForumLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.userForumLink) + "\">User Forum</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.userForumLink) + "\">User Forum</a></li>";
         }
         if (props.devForumLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.devForumLink) + "\">Dev Forum</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.devForumLink) + "\">Dev Forum</a></li>";
         }
         if (props.mailingListLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.mailingListLink) + "\">Mailing List</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.mailingListLink) + "\">Mailing List</a></li>";
         }
         if (props.chatLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.chatLink) + "\">Chat</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.chatLink) + "\">Chat</a></li>";
         }
         if (props.blogLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.blogLink) + "\">Blog</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.blogLink) + "\">Blog</a></li>";
         }
         if (props.twitterLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.twitterLink) + "\">Twitter</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.twitterLink) + "\">Twitter</a></li>";
         }
         if (props.issueTrackerLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.issueTrackerLink) + "\">Issue Tracker</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.issueTrackerLink) + "\">Issue Tracker</a></li>";
         }
         if (props.jiraLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.jiraLink) + "\">JIRA</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.jiraLink) + "\">JIRA</a></li>";
         }
         if (props.srcLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.srcLink) + "\">Source</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.srcLink) + "\">Source</a></li>";
         }
         if (props.anonymousLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.anonymousLink) + "\">Anonymous Source</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.anonymousLink) + "\">Anonymous Source</a></li>";
         }
         if (props.commiterLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.commiterLink) + "\">Committer Source Access</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.commiterLink) + "\">Committer Source Access</a></li>";
         }
         if (props.fisheyeLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.fisheyeLink) + "\">FishEye</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.fisheyeLink) + "\">FishEye</a></li>";
         }
         if (props.viewvcLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.viewvcLink) + "\">View Source</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.viewvcLink) + "\">View Source</a></li>";
         }
         if (props.githubLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.githubLink) + "\">Github</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.githubLink) + "\">Github</a></li>";
         }
         if (props.anonymousGitLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.anonymousGitLink) + "\">Anonymous Git</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.anonymousGitLink) + "\">Anonymous Git</a></li>";
         }
         if (props.committerGitLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.committerGitLink) + "\">Committer Git</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.committerGitLink) + "\">Committer Git</a></li>";
         }
         if (props.buildLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.buildLink) + "\">Build</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.buildLink) + "\">Build</a></li>";
         }
         if (props.hudsonLink) {
-          template += "<li><a href=\"" + getCorrectUrl(props.hudsonLink) + "\">Jenkins</a></li>";
+          template += "<li><a href=\"" + getCorrectUrl(props.sys_url_view, props.hudsonLink) + "\">Jenkins</a></li>";
         }
 
         template += "</ul></div><a class=\"upstream-toggle-more\" href=\"#\"><span class=\"view-more\">View More <i class=\"fa fa-plus-square\"></i></span><span class=\"view-less\">View Less <i class=\"fa fa-minus-square\"></i></span></a></li>";
