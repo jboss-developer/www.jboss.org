5cceffff6a97cf2b88cb128ba2c6e589a5efa022
wesbos <wesbos@gmail.com>
2014-06-04 12:17:38 -0400

    Fixing issue where isotope was being called before DCP returned items

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -183,9 +183,6 @@ app.init = function() {
   if($container.length) {
 
     $(window).load(function() {
-      $container.isotope({
-        itemSelector: '.buzz-item'
-      });
 
       // Filtering
       /*$('.sort-icons a').click(function(){
@@ -466,6 +463,14 @@ app.buzz = {
         html = "Sorry, no results to display.";
       }
       container.html(html);
+
+      // run isotope only once they have been embedded into the dom AND images are loaded
+      $(window).load(function() {
+        container.isotope({
+          itemSelector: '.buzz-item'
+        });
+      });
+
       container.removeClass('buzz-loading');
       $('.share-this').on('click mouseover', function() {
         Socialite.load($(this)[0]);
