f4f1fd18df3c5eb5568b52c73cc4ebaaa0684646
Pete Muir <pmuir@bleepbleep.org.uk>
2014-08-01 14:56:41 +0100

    Try turning waitReady off on web shims

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -5,7 +5,7 @@ interpolate: true
   Polyfill forms - input range sliders - for IE8 only
 */
 
-$.webshims.setOptions('basePath', '//static.jboss.org/www/vendor/web-shims-1.12.5/');
+$.webshims.setOptions({basePath: '//static.jboss.org/www/vendor/web-shims-1.12.5/', waitReady: false});
 $.webshims.polyfill('forms forms-ext');
 
 /* 
